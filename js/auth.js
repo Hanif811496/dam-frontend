6a2bdb93d9b018ed0b7366727256ab55845a7f89
nipodilo <habdillah2222@gmail.com>
2026-05-22 09:10:54 +0700

    add division to register and login

--- js/auth.js
+++ js/auth.js
@@ -27,3 +27,22 @@ function logout() {
   removeUser();
   window.location.href = "../pages/login.html";
 }
+
+function isManager() {
+  const user = getUser();
+  return user && user.division_id === "79732e94-d800-4b11-ad92-74e594f1b54b";
+}
+
+async function loadUserDivision() {
+  const user = getUser();
+  if (!user || user.division_id) return;
+  try {
+    const res  = await fetch(`${API_BASE_URL}/divisions/user/${user.user_id}`);
+    const data = await res.json();
+    if (data.division) {
+      user.division_id   = data.division.division_id;
+      user.division_nama = data.division.divisions?.nama || "";
+      saveUser(user);
+    }
+  } catch (e) { console.error(e); }
+}
